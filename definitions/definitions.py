9a495e1eee0b7771ea84bbae93a3dd93a3a20886
Josh Braun <josh.braun.4@gmail.com>
2024-03-29 17:12:38 -0400

    fix bug

--- definitions/definitions.py
+++ definitions/definitions.py
@@ -11,10 +11,6 @@ daily_refresh_schedule = ScheduleDefinition(
     job=define_asset_job(name="all_assets_job"), cron_schedule="0 0 * * *"
 )
 
-@asset()
-def test_asset():
-    pass
-
 defs = Definitions(
     assets=load_assets_from_package_module(assets), schedules=[daily_refresh_schedule]
 )
